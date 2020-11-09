6a2073985a843ab0f2b220ccfd4f050c86ee318f
thishonestabe <marquez.abraham1991@gmail.com>
2020-11-09 22:10:32 +0100

    Addded register and login to client controller

---
+++ api/client/client.controller.js
@@ -0,0 +1,52 @@
+const Client = require('./client.model');
+const Cita = require('../cita/cita.model')
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+require('dotenv/config');
+exports.login = async (req, res, next) => {
+    // let error = Validations.loginValidation(req.body);
+    // if(error) return res.status(400).send(error);
+    let user = await Client.findOne({ where:
+    {
+        email: req.body.email
+    }
+});
+    if(!user) return res.status(400).send('Invalid Credentials');
+    const validPass = await bcrypt.compare(req.body.password, user.password)
+    if(!validPass) return res.status(400).send('Invalid Credentials')
+
+    const token = jwt.sign({_id: user._id}, process.env.TOKEN_SECRET)
+    res.header('auth-token', token).send(token)
+
+};
+
+exports.register = async (req, res, next) => {
+    // let error = Validations.registerValidation(req.body);
+    let checkEmail = await Client.findOne({ where:
+    {
+        email: req.body.email
+    }
+});
+    if (checkEmail) return res.status(400).send('Email Already exists');
+    // if(!error) {
+
+        const salt = await bcrypt.genSalt(10);
+        const hashPassword = await bcrypt.hash(req.body.password, salt);
+        let user = {
+            name: req.body.name,
+            email: req.body.email,
+            password: hashPassword,
+            phone: req.body.phone
+        }
+        try {
+            const savedUser = await Client.create(user);
+            res.json({user:savedUser.name});
+        }catch (e) {
+            res.status(400).send(e);
+        }
+    // } else {
+    //     res.status(400).send(error);
+    // }
+}
+
+
