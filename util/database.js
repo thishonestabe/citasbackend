382ccbcf8c49306bda087cc8c552726cca83c937
thishonestabe <marquez.abraham1991@gmail.com>
2020-11-10 01:12:08 +0100

    added procfile for heroku and cloud db credentials

--- util/database.js
+++ util/database.js
@@ -1,8 +1,15 @@
 const Sequelize = require('sequelize');
 
-const sequelize = new Sequelize('citasdentist', 'root', '123456789', {
+// const sequelize = new Sequelize('citasdentist', 'root', '123456789', {
+//     dialect: 'mysql',
+//     host: 'localhost'
+// });
+
+const sequelize = new Sequelize('heroku_25527c9814913b6', 'b1c47c6845c37a', '983f0316', {
     dialect: 'mysql',
-    host: 'localhost'
+    host: 'eu-cdbr-west-03.cleardb.net'
 });
 
 module.exports = sequelize;
+
+
